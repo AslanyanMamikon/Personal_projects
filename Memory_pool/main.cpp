f124be0267bbd0597c349ff2f7cc2f7d1d3f8c48
AslanyanMamikon <mamikon.aslanyan90@gmail.com>
2025-03-13 23:19:31 +0400

    Update and rename main.cpp to Memory_pool/main.cpp

--- Memory_pool/main.cpp
+++ Memory_pool/main.cpp
@@ -24,7 +24,6 @@ int main() {
     int* p1 = pool.newElement(3);
     int* p2 = pool.newElement(3);
 
-
     std::cout << "Allocated addresses: " << p1 << ", " << p2 << std::endl;
     std::cout << "Allocated elements: " << *(p1) << ", " << *(p2) << std::endl;
 
@@ -42,14 +41,11 @@ int main() {
     pool.deleteElement(p3);
     pool.deleteElement(p4);
 
-    std::cout << "new test  *********************************************" << std::endl;
-
     MemoryPool<Test> pool2(10);
 
      Test* my_ptr = pool2.newElement(5, 6.6);
      std::cout << "Int ptr :" << my_ptr << std::endl;
      std::cout << "The elements : " << my_ptr->a << " , " << my_ptr->b << std::endl;
-
 
      pool2.deleteElement(my_ptr);
 

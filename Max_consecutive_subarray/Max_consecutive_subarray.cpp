45c3e12c584db9dd7e4283ea7139849c52b213a3
AslanyanMamikon <mamikon.aslanyan90@gmail.com>
2025-03-13 23:09:22 +0400

    Update and rename Max_consecutive_subarray.cpp to Max_consecutive_subarray/Max_consecutive_subarray.cpp

--- Max_consecutive_subarray/Max_consecutive_subarray.cpp
+++ Max_consecutive_subarray/Max_consecutive_subarray.cpp
@@ -4,7 +4,6 @@
 
 using namespace std;
 
-// Define a struct to replace tuple
 template <typename T>
 struct SubarrayResult {
 	int left;
@@ -55,21 +54,11 @@ SubarrayResult<T> Max_Subarray(vector<T>& arr, int low, int high)
 	}
 
 	int mid = (low + high) / 2;
-	//auto [leftLow, leftHigh, leftSum] = Max_Subarray(arr, low, mid);
-	//auto [rightLow, rightHigh, rightSum] = Max_Subarray(arr, mid + 1, high);
-	//auto [crossLow, crossHigh, crossSum] = Max_Cross_Subarray(arr, low, mid, high);
-
+	
 	auto left = Max_Subarray(arr, low, mid);
 	auto right = Max_Subarray(arr, mid + 1, high);
 	auto cross = Max_Cross_Subarray(arr, low, mid, high);
-
-	/*if (leftSum >= rightSum && leftSum >= crossSum)
-		return { leftLow, leftHigh,leftSum };
-	else if (rightSum >= leftSum && rightSum >= crossSum)
-		return { rightLow,rightHigh,rightSum };
-	else
-		return { crossLow,crossHigh,crossSum };*/
-
+	
 	if (left.sum >= right.sum && left.sum >= cross.sum)
 		return left;
 	else if (right.sum >= left.sum && right.sum >= cross.sum)
@@ -86,8 +75,6 @@ SubarrayResult<T> Max_Subarray_Sum(vector<T>& arr)
 	}
 	int n = arr.size();
 	auto result = Max_Subarray(arr, 0, n - 1);
-	//return get<2>(result);  // Since result is a tuple, we extract the third element (the max sum) using get<2>(result). // get<0>, get<1>, get<2>
-	//return result.sum;
 	return result;
 }
 
